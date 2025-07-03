b956e56de39c056987c1deb7a2987136f88e75d2
Souradeep Banerjee <rishibanerjee1101@gmail.com>
2025-07-03 20:21:46 +0530

    Add project files.

---
+++ Source/ProjectX/ProjectX.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectX : ModuleRules
+{
+	public ProjectX(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"ProjectX",
+			"ProjectX/Variant_Platforming",
+			"ProjectX/Variant_Combat",
+			"ProjectX/Variant_Combat/AI",
+			"ProjectX/Variant_SideScrolling",
+			"ProjectX/Variant_SideScrolling/Gameplay",
+			"ProjectX/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/ProjectX/ProjectXGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectXGameMode.h"
+
+AProjectXGameMode::AProjectXGameMode()
+{
+	// stub
+}
